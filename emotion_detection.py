12a671ad22d5880f4a2645d8434de6c0191772a3
WHITERABBlT <aantusch@gmail.com>
2025-08-22 10:09:32 -0400

    added packaging

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,11 +12,11 @@ def emotion_detector(text_to_analyze):
     dominant_emotion = max(emotions, key=emotions.get)
     
     output = {
-        'anger': emotions.get('anger'),
-        'disgust': emotions.get('disgust'),
-        'fear': emotions.get('fear'),
-        'joy': emotions.get('joy'),
-        'sadness': emotions.get('sadness'),
+        'anger': emotions.get('anger', 0),
+        'disgust': emotions.get('disgust', 0),
+        'fear': emotions.get('fear', 0),
+        'joy': emotions.get('joy', 0),
+        'sadness': emotions.get('sadness', 0),
         'dominant_emotion': dominant_emotion
     }
     return output
